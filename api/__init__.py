a3a1eb9c0daf6c88a37e65106b714bcbd93b7c15
Rahul Hoque <rhoque@umich.edu>
2025-03-09 22:26:23 -0400

    Update __init__

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,3 @@
 """
 __init__ for proper module initialization.
 """
-import main
-from main import main
-import driver
-from driver import Driver
-import scraper
-from scraper import Scraper
-import supabase_interface
-from supabase_interface import SupabaseInterface
